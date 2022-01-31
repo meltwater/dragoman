866773beee30cc9c579aaa5947868cfcb2e48a8a
Jeff Roche <jeff.roche@meltwater.com>
2022-01-31 14:37:57 -0500

    chore: Updating deploy pipeline

---
+++ scripts/ci/deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Install svu for version comparison
+go install github.com/caarlos0svu@1.9.0
+
+# Fetch the latest tags
+git fetch --tags
+
+CURRENT=$(svu current)
+NEXT=$(svu next)
+
+if [CURRENT != NEXT]
+then
+  echo "Tagging with" $NEXT
+  git tag $NEXT
+  git push --tags
+
+  # Do the release
+  go install github.com/goreleaser/goreleaser@v1.4.1
+  goreleaser --rm-dist
+else
+  echo "No new version detected. Skipping release."
+fi
